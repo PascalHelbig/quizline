f2a14ca2c43285937464f684fdaf0af8911b742d
PascalHelbig <pascalhelbig@gmail.com>
2016-02-25 13:16:46 +0100

    Neue Fragen anlegen

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
+
+const int MAX_CATEGORIES = 3;
 
 void insertQuestion(int ctg, const char* quest, const char* cor, const char* wr1, const char* wr2, const char* wr3) {
     cout << "inserted " << quest << endl;
@@ -30,7 +33,43 @@ int main() {
     do {
         cout << endl << "q - schließen" << endl;
         cout << "i - eintragen" << endl;
-        cin >> input;
+        getline(cin, input);
+
+        switch (input[0]) {
+            case 'i':
+                string question;
+                string answer;
+                string wrong1;
+                string wrong2;
+                string wrong3;
+                string categoryString;
+                int category;
+
+                cout << endl << "Bitte eingeben" << endl;
+                cout << "Deine Frage: ";
+                getline(cin, question);
+
+                cout << "Richtige Antwort: ";
+                getline(cin, answer);
+
+                cout << "Falsche Antwort 1: ";
+                getline(cin, wrong1);
+
+                cout << "Falsche Antwort 2: ";
+                getline(cin, wrong2);
+
+                cout << "Falsche Antwort 3: ";
+                getline(cin, wrong3);
+
+                do {
+                    cout << "Kategorie (1-" << MAX_CATEGORIES << "): ";
+                    getline(cin, categoryString);
+                    istringstream ss(categoryString);
+                    ss >> category;
+                } while (category < 1 || category > MAX_CATEGORIES);
+                insertQuestion(category, question.c_str(), answer.c_str(), wrong1.c_str(), wrong2.c_str(), wrong3.c_str());
+                break;
+        }
     } while(input != "q");
 
     //insertQuestion(0, "Wie alt ist...", "jung geblieben", "alt", "tot", "baby");
